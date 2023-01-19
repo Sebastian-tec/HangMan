9a6be3a78d63575723b5a9b830dc5c1647f5f880
WidovV <seba9188@elev.tec.dk>
2023-01-19 14:18:55 +0100

    Move layout prints into methods

---
+++ OutputMethods.cs
@@ -0,0 +1,142 @@
+﻿global using System;
+global using System.Collections.Generic;
+global using System.Linq;
+global using System.Text;
+global using System.Threading.Tasks;
+global using System.Diagnostics.Tracing;
+
+namespace HangMan
+{
+    public class OutputMethods
+    {
+        // Generate a random word from a list based on the length of the wordlength
+        public string RandomWord(int length)
+        {
+            Random random = new Random(); // Make use of the random class
+            string[] word = File.ReadAllLines(Path.Combine(Environment.CurrentDirectory, "word.txt")); // Read all the lines from the words.txt file
+            string randomWord = ""; // Get a random word from the list
+            // Create a for loop to generate a random word
+            do
+            {
+                randomWord = word[random.Next(0, word.Length)]; // Get a random word from the list
+            } while (randomWord.Length != length);
+
+            return randomWord; // Return the word
+        }
+
+        // Create a method to get the user input
+        public string GetLetter()
+        {
+            // Get a letter from the user and convert it to lowercase 
+            // This string declaration is probably not needed, and can be rewritten as "return Convert.ToString(Console.ReadKey().KeyChar).ToLower();"
+            string letter = Convert.ToString(Console.ReadKey().KeyChar).ToLower();
+
+            return letter; // Return the letter
+        }
+
+        // Create a method to check if the letter is in the word
+        public bool CheckLetter(string word, string letter)
+        {
+            // Check if the letter is in the word
+            if (word.Contains(letter))
+            {
+                return true; // Return true if the letter is in the word
+            }
+
+            return false; // Return false if the letter is not in the word
+        }
+
+        /* A void found on stackoverflow to clear the current line
+         * Basically just removes the typed char for a prettier console
+        */
+        public void ClearCurrentConsoleLine()
+        {
+            int currentLineCursor = Console.CursorTop;
+            Console.SetCursorPosition(0, Console.CursorTop);
+            Console.WriteLine(new string(' ', Console.WindowWidth));
+            Console.SetCursorPosition(0, currentLineCursor);
+        }
+
+        public void Layout()
+        {
+            Console.Clear();
+            Console.ForegroundColor = ConsoleColor.Magenta; // Set the foreground color to darkblue, just for fun
+            Console.WriteLine(@"
+  _   _                   __  __             
+ | | | | __ _ _ __   __ _|  \/  | __ _ _ __  
+ | |_| |/ _` | '_ \ / _` | |\/| |/ _` | '_ \ 
+ |  _  | (_| | | | | (_| | |  | | (_| | | | |
+ |_| |_|\__,_|_| |_|\__, |_|  |_|\__,_|_| |_|
+                    |___/                    
+"); // Write the title
+
+            Console.ForegroundColor = ConsoleColor.White; // Set the foreground color to white (so default)
+        }
+
+        public void HealthColor(int health)
+        {
+            if (health >= 0 && health <= 3)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                HealthBar(health);
+                Console.ForegroundColor = ConsoleColor.White;
+            }
+            else if (health > 3 && health <= 6)
+            {
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                HealthBar(health);
+                Console.ForegroundColor = ConsoleColor.White;
+            }
+            else if (health > 6 && health <= 10)
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                HealthBar(health);
+                Console.ForegroundColor = ConsoleColor.White;
+            }
+        }
+        public void HealthBar(int health)
+        {
+            Console.Write("Health: ");
+            if (health > 0)
+            {
+                for (int i = 0; i <= health; i++)
+                {
+                    Console.Write("♥ ");
+                }
+            }
+            else
+            {
+                Console.Write("X");
+            }
+
+            Console.WriteLine();
+        }
+
+        public void CurrentWord(string word, List<string> wordPrint, List<int> wordPos)
+        {
+            string print = "";
+            for (int i = 0; i < word.Length; i++)
+            {
+                print += "_";
+            }
+            Console.Write($"Current word: {print}"); // Write the word
+
+            if (wordPrint.Count > 0)
+            {
+                for (int i = 0; i < wordPrint.Count; i++)
+                {
+                    Console.SetCursorPosition(wordPos[i] + 14, Console.CursorTop); // Set the cursor position to the index of the letter + 14
+                    Console.Write($"{wordPrint[i]}"); // Write the letter(s)
+                }
+            }
+            Console.WriteLine(); // Add a little space
+        }
+
+        public void FullLayout(int health, string word, List<string> wordPrint, List<int> wordPos)
+        {
+            Layout();
+            HealthColor(health);
+            CurrentWord(word, wordPrint, wordPos);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,60 +1,78 @@
-﻿class Program
+﻿
+
+using HangMan;
+
+class Program
 {
     static void Main(string[] args)
     {
         do
         {
-            
-            OutPut op = new OutPut(); // Create a new instance of the HangMan class
+
+            OutputMethods op = new OutputMethods(); // Create a new instance of the HangMan class
             int length; // Create an int to hold the number of 
             int health = 10; // Create a int to hold the number of health/
             string word; // Create a string to hold the word
 
             Console.Title = "HangMan"; // Set the title of the window
             op.Layout(); // Call the Layout method
-            
+
             // Create a do-while loop to secure the user input
             do
             {
                 Console.Write("Word length: "); // Ask the user for the wordlength
-            } while (!int.TryParse(Console.ReadLine(), out length) || length > 26); // If the user input is not a number, ask again
-
+            } while (!int.TryParse(Console.ReadLine(), out length) || length > 14); // If the user input is not a number, ask again
 
 
             word = op.RandomWord(length); // Get a random word from the OutPut class
             List<int> wordPos = new List<int>(); // Create an array to store the word
             List<string> wordPrint = new List<string>(); // Create an array to store the word
-            Console.WriteLine(word); // Debug
+            // Console.WriteLine(word); // Debug
+            char[] letterlist = new char[word.Length]; // Create a char array to store the word
+            
+            // Create a for loop to fill the word array with "_"
+            for (int i = 0; i < word.Length; i++)
+            {
+                letterlist[i] = word[i]; // Add the word to the char array
+            }
+
 
             // Create a while loop to check if the user has any health left
             while (health > 0)
             {
+                op.FullLayout(health, word, wordPrint, wordPos);
                 string key = "";
                 do
                 {
+                    Console.WriteLine(word);
                     Console.WriteLine("Type a letter to guess the word"); // Ask for their guess
                     key = op.GetLetter(); // Save their guess in a string
                     op.ClearCurrentConsoleLine(); // Clear the line
                 } while (!key.All(char.IsLetter));
-                 
-                //Console.WriteLine($"Key: {key}"); // Debug
 
+
+                //Console.WriteLine($"Key: {key}"); // Debug
                 if (op.CheckLetter(word, key)) // Check if the letter is in the random word
                 {
-                    if (!wordPrint.Contains(key))
+                    for (int i = 0; i < word.Length; i++)
                     {
-                        // Console.WriteLine($"Index: {word.IndexOf(key)}"); // Debug
-                        wordPos.Add(word.IndexOf(key)); // Add the index of the letter to the list
-                                                        // Console.WriteLine($"Char: {key}"); // Debug
+                        
+                        if (letterlist[i].ToString() == key) // Check if the letter is in the word
+                        {
+                            // Console.WriteLine($"Index: {word.IndexOf(key)}"); // Debug
+                            wordPos.Add(word.IndexOf(key)); // Add the index of the letter to the list
+                            // Console.WriteLine($"Char: {key}"); // 
+                            wordPrint.Add(key); // Add the letter to the list
+                            letterlist[i] = ' '; // Remove the letter from the char array
 
-                        wordPrint.Add(key); // Add the letter to the list
-                        Console.WriteLine("Correct! The word contains the letter " + key); // Tell the user the letter is in the word
+                            Console.WriteLine("Correct! The word contains the letter " + key); // Tell the user the letter is in the word
+                        }
+                        else if (!letterlist.ToString().Contains(key) && wordPrint.Contains(key)) // Check if the letter is not in the word
+                        {
+                            Console.WriteLine($"You already guessed on {key}");
+                            health--;
+                        }
                     }
-                    else
-                    {
-                        Console.WriteLine($"You already guessed on {key}");
-                    }
-
                 }
                 else
                 {
@@ -63,44 +81,28 @@
                 }
                 // Console.WriteLine($"{wordPrint.Count} {wordPos.Count}"); // Debug
                 Thread.Sleep(1000);
-                if (wordPrint.Count > 0) // If the wordPrint list is not empty
-                {
-                    op.Layout();
-                    for (int j = 0; j < wordPrint.Count; j++) // Create a for loop based on the list-length
-                    {
-                        if (j == 0) // If the loop is on the first iteration
-                        {
-                            Console.WriteLine(); // Add a little space
-                            string print = "";
-                            for (int i = 0; i < word.Length; i++)
-                            {
-                                print += "_";
-                            }
-                            Console.Write($"Current word: {print}"); // Write the word
-                        }
 
-                        Console.SetCursorPosition(wordPos[j] + 14, Console.CursorTop); // Set the cursor position to the index of the letter + 14
-                        Console.Write($"{wordPrint[j]}"); // Write the letter(s)
-                    }
-                    Console.WriteLine(); // Add a little space
-                }
-                else
-                {
-                    op.Layout();
-                    Console.WriteLine(); // Add a little space
-                    string print = "";
-                    for (int i = 0; i < word.Length; i++)
-                    {
-                        print += "_";
-                    }
-                    Console.Write($"Current word: {print}"); // Write the word
-                    Console.WriteLine(); // Add a little space
-                }
 
-                Console.WriteLine($"Health left: {health}");
+                if (health == 0) // If their health is 0 (aka, they lost, noobs lol)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red; // Set the foreground color to red
+                    op.FullLayout(health, word, wordPrint, wordPos);
+                    
+                    Console.WriteLine("You ran out of health!"); // Tell the user they lost
+                    Console.WriteLine($"The word was {word} and you guessed {wordPrint.Count} letters correctly"); // Tell the user the word and how many letters they guessed correctly
+                    Console.ForegroundColor = ConsoleColor.White; // Set the foreground color to white (so default)
+                    Console.WriteLine("\nDo you want to play again? [Y/N]"); // Ask the user if they want to play again
+                    string choice = op.GetLetter(); // Save their choice in a string
+
+                    if (choice == "n") // If they want to play again
+                    {
+                        Environment.Exit(0); // Exit the program
+                    }
+                }
 
                 if (wordPrint.Count == word.Length)
                 {
+                    op.FullLayout(health, word, wordPrint, wordPos);
                     Console.ForegroundColor = ConsoleColor.Green; // Set the foreground color to green
                     Console.WriteLine("\nYou guessed the word!"); // Tell the user they guessed the word
                     Console.ForegroundColor = ConsoleColor.White; // Set the foreground color to white (so default)
@@ -110,95 +112,11 @@
                     {
                         Environment.Exit(0); // Exit the program
                     }
-                }
-
-                if (health == 0) // If their health is 0 (aka, they lost, noobs lol)
-                {
-                    Console.ForegroundColor = ConsoleColor.Red; // Set the foreground color to red
-                    Console.WriteLine("You ran out of health!"); // Tell the user they lost
-                    Console.WriteLine($"The word was {word} and you guessed {wordPrint.Count} letters correctly"); // Tell the user the word and how many letters they guessed correctly
-                    Console.ForegroundColor = ConsoleColor.White; // Set the foreground color to white (so default)
-                    Console.WriteLine("\nDo you want to play again? [Y/N]"); // Ask the user if they want to play again
-                    string choice = op.GetLetter(); // Save their choice in a string
-                    
-                    if (choice == "n") // If they want to play again
-                    {
-                        Environment.Exit(0); // Exit the program
-                    }
+                    health = 0;
                 }
 
                 Console.WriteLine(); // Add a little space
             }
         } while (true); // Loop the program forever (until the user exits)
-
-    }
-
-    // Create a new class to store methods
-    class OutPut 
-    {
-        // Generate a random word from a list based on the length of the wordlength
-        public string RandomWord(int length)
-        {
-            Random random = new Random(); // Make use of the random class
-            string[] word = File.ReadAllLines(Path.Combine(Environment.CurrentDirectory, "word.txt")); // Read all the lines from the words.txt file
-            string randomWord = ""; // Get a random word from the list
-            // Create a for loop to generate a random word
-            do
-            {
-                randomWord = word[random.Next(0, word.Length)]; // Get a random word from the list
-            } while (randomWord.Length != length);
-
-
-            return randomWord; // Return the word
-        }
-
-        // Create a method to get the user input
-        public string GetLetter()
-        {
-            // Get a letter from the user and convert it to lowercase 
-            // This string declaration is probably not needed, and can be rewritten as "return Convert.ToString(Console.ReadKey().KeyChar).ToLower();"
-            string letter = Convert.ToString(Console.ReadKey().KeyChar).ToLower();
-            
-            return letter; // Return the letter
-        }
-
-        // Create a method to check if the letter is in the word
-        public bool CheckLetter(string word, string letter)
-        {
-            // Check if the letter is in the word
-            if (word.Contains(letter))
-            {
-                return true; // Return true if the letter is in the word
-            }
-            
-            return false; // Return false if the letter is not in the word
-        }
-        
-        /* A void found on stackoverflow to clear the current line
-         * Basically just removes the typed char for a prettier console
-        */
-        public void ClearCurrentConsoleLine()
-        {
-            int currentLineCursor = Console.CursorTop;
-            Console.SetCursorPosition(0, Console.CursorTop);
-            Console.WriteLine(new string(' ', Console.WindowWidth));
-            Console.SetCursorPosition(0, currentLineCursor);
-        }
-
-        public void Layout()
-        {
-            Console.Clear();
-            Console.ForegroundColor = ConsoleColor.DarkBlue; // Set the foreground color to darkblue, just for fun
-            Console.WriteLine(@"
-  _   _                   __  __             
- | | | | __ _ _ __   __ _|  \/  | __ _ _ __  
- | |_| |/ _` | '_ \ / _` | |\/| |/ _` | '_ \ 
- |  _  | (_| | | | | (_| | |  | | (_| | | | |
- |_| |_|\__,_|_| |_|\__, |_|  |_|\__,_|_| |_|
-                    |___/                    
-"); // Write the title
-
-            Console.ForegroundColor = ConsoleColor.White; // Set the foreground color to white (so default)
-        }
     }
 }
