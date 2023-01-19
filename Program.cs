1f4bd84d24017b529b27f9a2da14d52cec5985da
WidovV <seba9188@elev.tec.dk>
2023-01-19 12:40:21 +0100

    Add a wordlist and choose the word from that

--- Program.cs
+++ Program.cs
@@ -10,24 +10,21 @@
             int health = 10; // Create a int to hold the number of health/
             string word; // Create a string to hold the word
 
-
-            Console.ForegroundColor = ConsoleColor.DarkBlue; // Set the foreground color to darkblue, just for fun
-            Console.WriteLine("HangMan\n"); // Write the title
             Console.Title = "HangMan"; // Set the title of the window
-            Console.ForegroundColor = ConsoleColor.White; // Set the foreground color to white (so default)
-
+            op.Layout(); // Call the Layout method
+            
             // Create a do-while loop to secure the user input
             do
             {
                 Console.Write("Word length: "); // Ask the user for the wordlength
-            } while (!int.TryParse(Console.ReadLine(), out length)); // If the user input is not a number, ask again
+            } while (!int.TryParse(Console.ReadLine(), out length) || length > 26); // If the user input is not a number, ask again
 
 
 
             word = op.RandomWord(length); // Get a random word from the OutPut class
             List<int> wordPos = new List<int>(); // Create an array to store the word
             List<string> wordPrint = new List<string>(); // Create an array to store the word
-            // Console.WriteLine(word); // Debug
+            Console.WriteLine(word); // Debug
 
             // Create a while loop to check if the user has any health left
             while (health > 0)
@@ -44,34 +41,59 @@
 
                 if (op.CheckLetter(word, key)) // Check if the letter is in the random word
                 {
-                    // Console.WriteLine($"Index: {word.IndexOf(key)}"); // Debug
-                    wordPos.Add(word.IndexOf(key)); // Add the index of the letter to the list
-                    // Console.WriteLine($"Char: {key}"); // Debug
-                    wordPrint.Add(key); // Add the letter to the list
-                    Console.WriteLine("Correct! The word contains the letter " + key); // Tell the user the letter is in the word
+                    if (!wordPrint.Contains(key))
+                    {
+                        // Console.WriteLine($"Index: {word.IndexOf(key)}"); // Debug
+                        wordPos.Add(word.IndexOf(key)); // Add the index of the letter to the list
+                                                        // Console.WriteLine($"Char: {key}"); // Debug
+
+                        wordPrint.Add(key); // Add the letter to the list
+                        Console.WriteLine("Correct! The word contains the letter " + key); // Tell the user the letter is in the word
+                    }
+                    else
+                    {
+                        Console.WriteLine($"You already guessed on {key}");
+                    }
+
                 }
                 else
                 {
                     Console.WriteLine("Incorrect! The word does not contain the letter " + key); // Tell the user the letter is not in the word
                     health--; // Remove one from the health
-
                 }
                 // Console.WriteLine($"{wordPrint.Count} {wordPos.Count}"); // Debug
-
+                Thread.Sleep(1000);
                 if (wordPrint.Count > 0) // If the wordPrint list is not empty
                 {
+                    op.Layout();
                     for (int j = 0; j < wordPrint.Count; j++) // Create a for loop based on the list-length
                     {
-                        
                         if (j == 0) // If the loop is on the first iteration
                         {
                             Console.WriteLine(); // Add a little space
-                            Console.Write($"Current word: "); // Write the word
+                            string print = "";
+                            for (int i = 0; i < word.Length; i++)
+                            {
+                                print += "_";
+                            }
+                            Console.Write($"Current word: {print}"); // Write the word
                         }
 
                         Console.SetCursorPosition(wordPos[j] + 14, Console.CursorTop); // Set the cursor position to the index of the letter + 14
                         Console.Write($"{wordPrint[j]}"); // Write the letter(s)
                     }
+                    Console.WriteLine(); // Add a little space
+                }
+                else
+                {
+                    op.Layout();
+                    Console.WriteLine(); // Add a little space
+                    string print = "";
+                    for (int i = 0; i < word.Length; i++)
+                    {
+                        print += "_";
+                    }
+                    Console.Write($"Current word: {print}"); // Write the word
                     Console.WriteLine(); // Add a little space
                 }
 
@@ -98,6 +120,7 @@
                     Console.ForegroundColor = ConsoleColor.White; // Set the foreground color to white (so default)
                     Console.WriteLine("\nDo you want to play again? [Y/N]"); // Ask the user if they want to play again
                     string choice = op.GetLetter(); // Save their choice in a string
+                    
                     if (choice == "n") // If they want to play again
                     {
                         Environment.Exit(0); // Exit the program
@@ -116,21 +139,17 @@
         // Generate a random word from a list based on the length of the wordlength
         public string RandomWord(int length)
         {
-            // Create a "empty" string 
-            // It's done this way since c# doesn't allow me to return the string without an declaration in the same scope
-            string word = "";
-            string alphabet = "abcdefghijklmnopqrstuvwxyz"; // Create a list of the alphabet
-            
             Random random = new Random(); // Make use of the random class
-
+            string[] word = File.ReadAllLines(Path.Combine(Environment.CurrentDirectory, "word.txt")); // Read all the lines from the words.txt file
+            string randomWord = ""; // Get a random word from the list
             // Create a for loop to generate a random word
-            for (int i = 0; i < length; i++)
+            do
             {
-                int index = random.Next(alphabet.Length); // Get a random number between 0 and 25
-                word += alphabet[index]; // Add the letter to the word based on the random index
-            }
-            
-            return word; // Return the word
+                randomWord = word[random.Next(0, word.Length)]; // Get a random word from the list
+            } while (randomWord.Length != length);
+
+
+            return randomWord; // Return the word
         }
 
         // Create a method to get the user input
@@ -164,6 +183,22 @@
             Console.SetCursorPosition(0, Console.CursorTop);
             Console.WriteLine(new string(' ', Console.WindowWidth));
             Console.SetCursorPosition(0, currentLineCursor);
+        }
+
+        public void Layout()
+        {
+            Console.Clear();
+            Console.ForegroundColor = ConsoleColor.DarkBlue; // Set the foreground color to darkblue, just for fun
+            Console.WriteLine(@"
+  _   _                   __  __             
+ | | | | __ _ _ __   __ _|  \/  | __ _ _ __  
+ | |_| |/ _` | '_ \ / _` | |\/| |/ _` | '_ \ 
+ |  _  | (_| | | | | (_| | |  | | (_| | | | |
+ |_| |_|\__,_|_| |_|\__, |_|  |_|\__,_|_| |_|
+                    |___/                    
+"); // Write the title
+
+            Console.ForegroundColor = ConsoleColor.White; // Set the foreground color to white (so default)
         }
     }
 }
